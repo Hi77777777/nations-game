b6ab359d844a7198812a2e6294648165efc0bcad
Your Name <you@example.com>
2025-09-27 16:45:49 -0400

    random walk

---
+++ character_body_2d.gd
@@ -0,0 +1,52 @@
+extends CharacterBody2D
+
+var tile_position := Vector2i(0, 0)
+var time_passed := 0
+var prev_position = null
+
+func move_to_random_neighbour():
+	var neighbours := []
+	var tilemap = $"../TileMapLayer"
+	# Directions to check: up, down, left, right + some extended neighbours
+	var directions = [
+		Vector2i(0, -1),
+		Vector2i(0, 1),
+		Vector2i(-1, 0),
+		Vector2i(1, 0),
+		Vector2i(1, 1),
+		Vector2i(-1, 1)
+	]
+
+	for dir in directions:
+		var neighbour_pos = tile_position + dir
+		if tilemap.get_used_cells().has(neighbour_pos) and neighbour_pos != prev_position:
+			var neighbour_id = tilemap.get_cell_source_id(neighbour_pos)
+			neighbours.append({
+				"position": neighbour_pos,
+				"id": neighbour_id
+			})
+	
+	prev_position = tile_position
+	
+	var random_neighbour = neighbours.pick_random()
+	
+	tile_position = random_neighbour["position"]
+	self.position = tilemap.to_global(tilemap.map_to_local(random_neighbour["position"]))
+
+func _process(delta: float) -> void:
+	time_passed += 1
+	if time_passed % 60 == 0:
+		move_to_random_neighbour()
+
+func _ready() -> void:
+	var tilemap = $"../TileMapLayer"
+	var used_cells = tilemap.get_used_cells()
+	
+	if used_cells.is_empty():
+		return
+
+	var first_cell = used_cells[0]
+	tile_position = first_cell
+
+	# Move the character to that tile's world position
+	self.position = tilemap.to_global(tilemap.map_to_local(first_cell))
