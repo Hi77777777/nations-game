176db4b588a7dee4871aec63251941788baf9f54
Heinrich Xiao <74563446+Heinrich-XIAO@users.noreply.github.com>
2025-09-27 22:28:56 -0400

    asdf

--- click.gd
+++ click.gd
@@ -1,10 +1,30 @@
 extends Area2D
 
+@onready var sprite_texture = load("res://images/red_outline.png")
+
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and Selections.current_selection_mode == "none":
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			$"../YellowOutline".visible = true
 			Selections.current_selection_mode = "to"
+			for cell in $"..".get_can_go():
+				create_sprite($"../../TileMapLayer".to_global($"../../TileMapLayer".map_to_local(cell)))
 
 func _ready() -> void:
 	$"../YellowOutline".visible = false
+	create_sprite(Vector2(0, 0))
+
+func create_sprite(position: Vector2) -> void:
+	# Create the Sprite2D node
+	var sprite = Sprite2D.new()
+	
+	# Set the sprite's texture
+	sprite.texture = sprite_texture
+	
+	# Position the sprite at the mouse click position
+	sprite.position = position
+	
+	sprite.scale = Vector2(1,1)
+	
+	# Add the sprite to the scene
+	$"../..".add_child(sprite)
